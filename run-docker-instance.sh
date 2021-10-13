b92c190bde0775579b60c0999134ac79782d08a6
Bill Meyer <iambillmeyer@gmail.com>
2021-10-13 08:26:21 -0500

    Added additional variables for DBPORT, HTTPHOST, and HTTPPORT

--- run-docker-instance.sh
+++ run-docker-instance.sh
@@ -2,10 +2,13 @@
 
 source config.sh
 
+# See config.sh for setting these values.
 docker run -it -p 8080:8080 \
 	-e SPLUNK_ACCESS_TOKEN=${SPLUNK_ACCESS_TOKEN} \
 	-e SPLUNK_REALM=${SPLUNK_REALM} \
 	-e DBHOST=${DBHOST} \
-	-e DBPORT=3306 \
+	-e DBPORT=${DBPORT} \
+	-e HTTPHOST=${HTTPHOST} \
+	-e HTTPPORT=${HTTPPORT} \
 	nationalparks-rest
 
